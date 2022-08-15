cb4b0345f1b08560d8897d3e2d5ae5028d92d871
Shirshak <shs1233@my.londonmet.ac.uk>
2022-08-15 22:36:34 +0545

    pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -40,6 +40,12 @@ const getAllProducts = async (req, res) => {
     result = result.sort("createdAt");
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
   res.status(StatusCodes.OK).json({ products, count: products.length });
