2e0cef90ffb094591f5413a4fa959a9eb896fff1
Shirshak <shs1233@my.londonmet.ac.uk>
2022-08-16 22:49:16 +0545

    pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -41,14 +41,19 @@ const getAllProducts = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 6;
   const skip = (page - 1) * limit;
+
+  const totalProducts = await Product.estimatedDocumentCount();
+  let totalPages = Math.ceil(totalProducts / limit);
 
   result = result.skip(skip).limit(limit);
 
   const products = await result;
 
-  res.status(StatusCodes.OK).json({ products, count: products.length });
+  res
+    .status(StatusCodes.OK)
+    .json({ products, count: products.length, totalPages });
 };
 
 const getSingleProduct = async (req, res) => {
