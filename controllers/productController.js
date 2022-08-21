7c366028fb2263a60dcd5d2ffdbf142be0e9069c
Shirshak <shs1233@my.londonmet.ac.uk>
2022-08-21 15:14:40 +0545

    fixed pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -33,19 +33,20 @@ const getAllProducts = async (req, res) => {
 
   let result = Product.find(queryObject);
 
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 6;
+  const skip = (page - 1) * limit;
+
+  const totalProducts = await Product.countDocuments(result);
+  let totalPages = Math.ceil(totalProducts / limit);
+
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
-
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 6;
-  const skip = (page - 1) * limit;
-
-  const totalProducts = await Product.estimatedDocumentCount();
-  let totalPages = Math.ceil(totalProducts / limit);
 
   result = result.skip(skip).limit(limit);
 
